4545dc7dba42ab490b33adaef640be53140546cf
Niranga <nirangak95@gmail.com>
2026-04-16 11:37:10 +0530

    1

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-const API_BASE = import.meta.env.VITE_API_URL;
-
 export default defineConfig({
   plugins: [react()],
   server: {
