71a75b17914818f6c4dc1301e3f535de7049b546
Niranga <nirangak95@gmail.com>
2026-06-06 08:23:22 +0530

    add

--- vite.config.js
+++ vite.config.js
@@ -8,13 +8,13 @@ export default defineConfig({
     port: 5173,
     strictPort: true,
     hmr: {
-      host: '10.231.32.253',
+      host: '10.231.32.38',
       protocol: 'ws',
       port: 5173,
     },
     proxy: {
       '/api': {
-        target: 'http://10.231.32.253:3000',
+        target: 'http://10.231.32.38:3000',
         changeOrigin: true,
         secure: false,
       },
